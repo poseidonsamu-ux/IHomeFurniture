2e6cb96fafc7ffea536d758621ed81eb72de1a48
NguyenLeGiaBao <posseidonsamu@gmail.com>
2026-04-21 22:27:31 +0700

    update HomeController hiển thị 8 sản phẩm ra ngoài index

--- IHomeFurniture/Controllers/HomeController.cs
+++ IHomeFurniture/Controllers/HomeController.cs
@@ -3,27 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IHomeFurniture.Models;
 
 namespace IHomeFurniture.Controllers
 {
     public class HomeController : Controller
     {
+        IHomeFurnitureEntities db = new IHomeFurnitureEntities();
+
         public ActionResult Index()
         {
-            return View();
+            // 2. Lấy 8 sản phẩm mới nhất (sắp xếp giảm dần theo ngày cập nhật hoặc mã SP)
+            // Sếp có thể đổi s.MaSP thành s.NgayCapNhat nếu trong Database có cột đó nha
+            var sanPhamMoi = db.SANPHAMs.OrderByDescending(s => s.MaSP).Take(8).ToList();
+
+            // 3. Đẩy danh sách 8 sản phẩm này ra ngoài giao diện Index.cshtml
+            return View(sanPhamMoi);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
