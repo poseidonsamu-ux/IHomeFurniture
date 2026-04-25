7558b4037edfe56fcd053c55fe030b467325b80e
hihoa0936-eng <hihoa0936@gmail.com>
2026-04-25 14:24:17 +0700

    Thêm trang giỏ hàng , sản phẩm , liên hệ

---
+++ IHomeFurniture/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace IHomeFurniture.Models
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice => Price * Quantity; // Tính tổng tiền của món đó
+    }
+}
